783f251ee7174e169fb6b04ce3743a5366c0eb0d
ShayonX <ShayonX@users.noreply.github.com>
2018-04-22 22:01:58 -0600

    Classifier Trained

--- crowd_density_classifier.py
+++ crowd_density_classifier.py
@@ -4,117 +4,68 @@ import keras
 import numpy as np
 from PIL import Image
 from numpy import array
-
-from keras.datasets import mnist
-
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from sklearn.preprocessing import LabelEncoder
+from keras.utils import np_utils
 from keras.models import Sequential
-
-from keras.layers import Dense, Dropout, Flatten
-
+from keras.layers import Dense, Dropout, Flatten, Activation
 from keras.layers import Conv2D, MaxPooling2D
-
 from keras import backend as K
 import os
+import json
+
 batch_size = 128
 
-num_classes = 10
+num_classes = 5
 
-epochs = 12
+epochs = 10
 
 # input image dimensions
 
-img_rows, img_cols = 28, 28
+img_rows, img_cols = 576, 768
 
-x_train=array([])
 # the data, split between train and test sets
-for i in os.listdir('Data/'):
-    img = Image.open(i)
-    x_train = np.append(x_train,array(img))
+X = []
+for i in zip(os.listdir('Data/'), range(0, 754)):
+    img = load_img("Data/" + i[0])
+    X.append(img_to_array(img))
+X = np.asarray(X)
 
+Y = []
+classes = {}
+with open("Labels/numeric_classes.json") as a:
+    classes = json.load(a)
+Y = list((classes.values()))
+encoder = LabelEncoder()
+encoder.fit(Y)
+encoded_Y = encoder.transform(Y)
+Y = np_utils.to_categorical(encoded_Y)
 
-y_train=[]
-classes={}
-with open("Labels/numeric_classes") as a:
-    classes=json.load(a)
-y_train =classes.values()
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=0)
 
-
-
-# (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-
-if K.image_data_format() == 'channels_first':
-
-    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-
-    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-
-    input_shape = (1, img_rows, img_cols)
-
-else:
-
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-
-    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-
-    input_shape = (img_rows, img_cols, 1)
-
-x_train = x_train.astype('float32')
-
-x_test = x_test.astype('float32')
-
-x_train /= 255
-
-x_test /= 255
-
-print('x_train shape:', x_train.shape)
-
-print(x_train.shape[0], 'train samples')
-
-print(x_test.shape[0], 'test samples')
-
-# convert class vectors to binary class matrices
-
-y_train = keras.utils.to_categorical(y_train, num_classes)
-
-y_test = keras.utils.to_categorical(y_test, num_classes)
-
+input_shape = (576, 768, 3)
 model = Sequential()
 
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
-
-model.add(Conv2D(64, (3, 3), activation='relu'))
-
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Dropout(0.25))
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())
 
-model.add(Dense(128, activation='relu'))
+model.add(Dense(output_dim=64))
+model.add(Activation("relu"))
 
-model.add(Dropout(0.5))
+model.add(Dense(output_dim=5))
+model.add(Activation("softmax"))
+model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
 
-model.add(Dense(num_classes, activation='softmax'))
-
-model.compile(loss=keras.losses.categorical_crossentropy,
-
-              optimizer=keras.optimizers.Adadelta(),
-
-              metrics=['accuracy'])
-
-model.fit(x_train, y_train,
-
-          batch_size=batch_size,
-
-          epochs=epochs,
-
-          verbose=1,
-
-          validation_data=(x_test, y_test))
-
-score = model.evaluate(x_test, y_test, verbose=0)
-
-print('Test loss:', score[0])
-
-print('Test accuracy:', score[1])
+model.fit(x_train, y_train, epochs=epochs)
