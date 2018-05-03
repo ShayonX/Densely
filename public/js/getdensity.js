fed91ccf370566cca67a08cc057bede1fd315a02
Prashil Bhimani <prashilbhimani24@gmail.com>
2018-05-03 08:29:56 -0600

    Updated UI

--- public/js/getdensity.js
+++ public/js/getdensity.js
@@ -1,9 +1,9 @@
 function ajaxDensity() {
   $.get( '/getDensity', function(data) {
-  	var div= "<tr>";
+  	var div= "<tr scope='row'>";
   	data=JSON.parse(data);
   	var frame=data["frameno"];
-  	var img= "<td><img src='http://localhost:8000/data/frame"+frame+".jpg' height='200' width='200'></td>";
+  	var img= "<td align='center'><img src='http://localhost:8000/data/frame"+frame+".jpg' height='200' width='200'></td>";
   	div=div+img;
 	div=div+"<td>"+data["people"]+"</td>";
 	div=div+"</tr>";
--- test.py
+++
@@ -1,4 +0,0 @@
-import sys
-#print(sys.argv[1])
-print("hi")
-sys.stdout.flush()
